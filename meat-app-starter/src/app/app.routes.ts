2b7c37c50f2a925809d1a68923d34968a34e46a5
dfklein <dklein@gmail.com>
2018-06-23 16:41:31 -0300

    Fim da aula 46

--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import {Routes} from '@angular/router'
 import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 
 export const ROUTES: Routes = [
   {path:'', component: HomeComponent}, // toda vez que você utilizar um componente você precisa importá-lo. Ao definir as rotas você também precisa importá-lo no módulo principal (ver app.module.ts)
   {path:'about', component: AboutComponent},
-  {path:'restaurants', component: RestaurantsComponent}
+  {path:'restaurants', component: RestaurantsComponent},
+  {path:'restaurants/:id', component: RestaurantDetailComponent}
 ]
