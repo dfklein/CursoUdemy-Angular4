14ca759c2b3782fde948635339605e29123392d4
dfklein <dklein@gmail.com>
2018-07-08 10:24:41 -0300

    Aula 49

--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -4,10 +4,17 @@ import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
+import { MenuComponent } from './restaurant-detail/menu/menu.component'
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 export const ROUTES: Routes = [
   {path:'', component: HomeComponent}, // toda vez que você utilizar um componente você precisa importá-lo. Ao definir as rotas você também precisa importá-lo no módulo principal (ver app.module.ts)
   {path:'about', component: AboutComponent},
   {path:'restaurants', component: RestaurantsComponent},
-  {path:'restaurants/:id', component: RestaurantDetailComponent}
+  {path:'restaurants/:id', component: RestaurantDetailComponent,
+    children: [
+      {path: '', redirectTo: 'menu', pathMatch:'full'}, // path match full quer dizer que o redirecionamento funcionará apenas para esta URL especificamente.
+      {path: 'menu', component: MenuComponent},
+      {path: 'review', component: ReviewsComponent} // veja em restaurant-detail.component.html que você declarou outro router-outlet, que é para comportar o conteúdo das rotas filhas.
+    ]}
 ]
